fcf701bd3a992d631899a668c7573e1a1df36825
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-28 21:13:49 -0500

    push update

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
+// video 1√
+// video 2√
+// video 3√
+// video 4√
+// video 5
+
+
+
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 
-    printf("Hello World");
+
+
+    
+    int age = 22; 
+    char name[] = "Luke"; // Since a string is an object, and C isn't object oriented, we need a string of characters to create a string
+
+
+    printf("You're name is %s - you are %d", name, age);
+
 
     return 0;
 }
